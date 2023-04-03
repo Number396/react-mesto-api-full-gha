90c6b410a2cb1e3b6ae75926ed0af55704d361a1
Number396 <number396@yandex.ru>
2023-04-03 21:00:43 +0300

    refactor: change api url to *monster

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,13 +1,13 @@
 export const apiConfig = {
     // url: "http://130.193.51.95:3000",
-    url: "http://api.mesto.number396.nomoredomains.work",
+    url: "http://api.mesto.number396.nomoredomains.monster",
     headers: {
         "Content-Type": "application/json",
     },
 };
 // url: "https://auth.nomoreparties.co/",
 export const apiAuthConfig = {
-    url: "http://api.mesto.number396.nomoredomains.work/",
+    url: "http://api.mesto.number396.nomoredomains.monster",
     headers: {
         "Content-Type": "application/json"
     }
